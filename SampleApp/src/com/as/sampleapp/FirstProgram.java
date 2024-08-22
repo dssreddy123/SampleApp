861ea814ae415c027d795b20959323c9fa3dd6e3
Syama SRD <dssreddy123@gmail.com>
2024-08-21 18:24:52 -0700

    Create FirstProgram.java

---
+++ SampleApp/src/com/as/sampleapp/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.as.sampleapp;
+
+public class FirstProgram {
+        public static void main(String[] args) {
+                System.out.println("It is sample application");
+                addMethod(32,43);
+        }
+        public static void addMethod (int a, int b){
+
+               System.out.println("Total : " + (a+b));
+        }
+}
