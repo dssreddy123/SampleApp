d841a0083b6a94934e29c6efa91882668c3412f2
Syama SRD <dssreddy123@gmail.com>
2024-08-21 18:58:34 -0700

    Update SampleApplication.java

--- SampleApp/src/com/as/sampleapp/SampleApplication.java
+++ SampleApp/src/com/as/sampleapp/SampleApplication.java
@@ -3,9 +3,9 @@ package com.as.sampleapp;
 public class SampleApplication {
 	public static void main(String[] args) {
                 System.out.println("It is sample application");
-                add(32,43);
+                addExtraMethod(32,43);
         }
-        public static void add (int a, int b){
+        public static void addExtraMethod (int a, int b){
 
                System.out.println("Total : " + (a+b));
         }
